24f3dea7ad921ca2074cf11c1c7f850f130818ce
Saurin Shah <sourin.shah@gmail.com>
2018-05-19 19:40:39 -0400

    PR1 submission

---
+++ cs5200-summer2018-shah/src/main/java/edu/northeastern/cs5200/controllers/hello/HelloObject.java
@@ -0,0 +1,22 @@
+package edu.northeastern.cs5200.controllers.hello;
+
+public class HelloObject {
+	
+	private String message;
+	
+	public HelloObject() {
+	}
+	
+	public HelloObject(String message) {
+		this.message = message;
+	}
+	
+	public String getMessage() {
+		return message;
+	}
+	
+	public void setMessage(String message) {
+		this.message = message;
+	}
+	
+}
